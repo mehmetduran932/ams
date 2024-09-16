c8cec1fb557f6b69c40564492805d24b7ee2c856
Pinsoft-Mehmet <7410852Mm>
2024-09-16 17:49:29 +0300

    update

--- src/config/dbConnection.js
+++ src/config/dbConnection.js
@@ -1,8 +1,5 @@
 const mongoose=require("mongoose")
-mongoose.connect(process.env.DB_CONNECTION_STRING,{
-    useNewUrlParser:true,
-    useUnifiedTopology:true
-})
+mongoose.connect(process.env.DB_CONNECTION_STRING)
 
 .then(()=>{
     console.log("Db Bağlantısı Ok.")
